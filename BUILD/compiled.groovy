08ba617a70296f2369f29b632029e5a2d5c1cfb1
shubham <shubham.devops.cloud@gmail.com>
2024-07-15 20:41:44 +0530

    added elasticserach pipeline

--- BUILD/compiled.groovy
+++ BUILD/compiled.groovy
@@ -1,14 +1,34 @@
+// Multibranch Pipeline for Databases
+
 String folderroot = "BUILD-JOBS"
 folder(folderroot){
 
 }
 
-// Multibranch Pipeline for Databases
+// For MySql
 multibranchPipelineJob(folderroot + '/' + 'mysql'){
     branchSources{
         git{
             id('1') // IMPORTANT: use a constant and unique identifier
             remote('git@github.com:shubham-devops-cloud/mysql.git')
+            credentialsId('GitHubSSH')
+            includes('main')
+            excludes('')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(6)
+        }
+    }
+}
+
+// For Elasticsearch
+multibranchPipelineJob(folderroot + '/' + 'elasticsearch'){
+    branchSources{
+        git{
+            id('1') // IMPORTANT: use a constant and unique identifier
+            remote('git@github.com:shubham-devops-cloud/elasticsearch.git')
             credentialsId('GitHubSSH')
             includes('main')
             excludes('')
