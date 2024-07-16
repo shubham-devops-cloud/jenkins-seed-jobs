6dabd92444ee5ce4ea659393a61d4c9a229bffd5
shubham <shubham.devops.cloud@gmail.com>
2024-07-16 21:18:46 +0530

    update

--- BUILD/compiled.groovy
+++ BUILD/compiled.groovy
@@ -1,7 +1,6 @@
 String folderroot = "BUILD-JOBS"
 
 folder(folderroot) {
-    // Create jobs using the function
     createMultibranchPipelineJob(folderroot, 'mysql', 'git@github.com:shubham-devops-cloud/mysql.git')
     createMultibranchPipelineJob(folderroot, 'elasticsearch', 'git@github.com:shubham-devops-cloud/elasticsearch.git')
     createMultibranchPipelineJob(folderroot, 'attendance', 'git@github.com:shubham-devops-cloud/attendance.git')
