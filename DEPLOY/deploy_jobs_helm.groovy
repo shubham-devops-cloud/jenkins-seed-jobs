abaea5e95c81a320472b9492868e299c3e2d0d68
shubham <shubham.devops.cloud@gmail.com>
2024-08-04 13:44:23 +0530

    update

--- DEPLOY/deploy_jobs_helm.groovy
+++ DEPLOY/deploy_jobs_helm.groovy
@@ -8,11 +8,25 @@ folder(folderroot){
     //displayName("HELM")
 }
 
-jobs.createJobWithBranchExtension(pipelineJob(folderroot + "/deploy_salary"), "es-charts-salary", "main", fileName = "Jenkinsfile.deploy").parameters{
-    choiceParam('REPONAME', ['es-charts-salary'], 'Helm chart repo name')
+
+//Pipeline for MySql
+jobs.createJobWithBranchExtension(pipelineJob(folderroot + "/deploy_mysql"), "es-charts-mysql", "main", fileName = "Jenkinsfile.deploy").parameters{
+    choiceParam('REPONAME', ['es-charts-mysql'], 'Helm chart repo name')
     textParam{
         name('FEATUREIMAGE')
-        defaultValue('public.ecr.aws/j9k0i2s2/dev-or-employee-system:salary-main-0.0.1')
+        defaultValue('public.ecr.aws/j9k0i2s2/dev-or-employee-system:mysql-main-0.0.1')
+        description('Enter the image name which you want to deploy')
+    }
+    choiceParam('NAMESPACE', ['dev'], 'Kubernetes Namespace')
+}
+
+
+//Pipeline for Elasticsearch
+jobs.createJobWithBranchExtension(pipelineJob(folderroot + "/deploy_elasticsearch"), "es-charts-elasticsearch", "main", fileName = "Jenkinsfile.deploy").parameters{
+    choiceParam('REPONAME', ['es-charts-elasticsearch'], 'Helm chart repo name')
+    textParam{
+        name('FEATUREIMAGE')
+        defaultValue('public.ecr.aws/j9k0i2s2/dev-or-employee-system:elasticsearch-main-0.0.1')
         description('Enter the image name which you want to deploy')
     }
     choiceParam('NAMESPACE', ['dev'], 'Kubernetes Namespace')
